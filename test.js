d6bb03bedf782b654c2ead3e2fa421dfecee02fa
Nicolas <nicolas.meseth@googlemail.com>
2018-05-28 17:16:35 +0200

    Small fixes

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ var data = {
 }
 
 // Set API Key here
-ml.setAPIKey("yz08LlHadvd9w4sDzdq/kRUXpOVI7+MwHoq9DWS8CPFbJ0nVQ12VUf1ShIzq6aedHFL/YssZkA9iEeRI/LxcSA==");
+ml.setAPIKey("<your-api-key-here>");
 
 var hostname = "ussouthcentral.services.azureml.net";
 var path = "/workspaces/d6591b016ab34582b55ef6d3fea46aed/services/a6fc3256145b4b33a0657ebe439e3f8e/execute?api-version=2.0&details=true"
